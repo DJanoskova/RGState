f611fbb0b9ab36cc9671ade2eec856b8a7dc352e
Dana Janoskova <info@danajanoskova.sk>
2022-10-03 14:41:34 +0200

    do not access local storage if window is not defined

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,12 +1,19 @@
 const STATE_NAME = '_rgstate_local_storage'
 
 export const getStoredState = () => {
+  if (typeof window === 'undefined') {
+    return;
+  }
   const value = localStorage.getItem(STATE_NAME);
   if (value) return JSON.parse(value);
   return {};
 }
 
 export const setStoredState = (name: string, value: any) => {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
   const stored = getStoredState();
   stored[name] = value;
   localStorage.setItem(STATE_NAME, JSON.stringify(stored));
